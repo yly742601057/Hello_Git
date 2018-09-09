26160f3e486b5710a78c0a3360e08f294a7e7851
liangyou.yuan <yly742601057@163.com>
2018-09-09 16:30:29 +0800

    First commit all.

---
+++ src/com/yly/hello/HelloGit.java
@@ -0,0 +1,11 @@
+package com.yly.hello;
+
+/**
+ * @author: LiangYou.Yuan
+ * @create: 2018-09-09 16:28
+ */
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Git, I come from IDEA");
+	}
+}
